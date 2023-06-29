0d12c9e51e4683948e9913bfc7542ae56a392da2
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-06-29 16:53:48 -0600

    Update documentation

--- parallel_pick_behaviors/include/parallel_pick_behaviors/wait_and_pop_solution_queue.hpp
+++ parallel_pick_behaviors/include/parallel_pick_behaviors/wait_and_pop_solution_queue.hpp
@@ -14,10 +14,10 @@ namespace moveit_studio::behaviors
  * @brief Pops the solution queue to get the next solution to be processed. If the queue is empty it will return RUNNING
  *
  * @details
- * | Data Port Name   | Port Type | Object Type                                     |
- * |------------------|-----------|-------------------------------------------------|
- * | solution queue   | Input     | std::queue                                      |
- * | solution         | Output    | moveit_task_constructor_msgs::msg::Solution     |
+ * | Data Port Name   | Port Type | Object Type                                             |
+ * |------------------|-----------|---------------------------------------------------------|
+ * | solution queue   | Input     | std::queue<moveit_task_constructor_msgs::msg::Solution> |
+ * | solution         | Output    | moveit_task_constructor_msgs::msg::Solution             |
  */
 class WaitAndPopSolutionQueue final : public AsyncBehaviorBase
 {
@@ -29,7 +29,7 @@ public:
 
 private:
   /**
-   * @brief Read queue from the blackboard, check wether or not it is empty and pop the next solution if it is not
+   * @brief Read queue from the blackboard, check whether or not it is empty and pop the next solution if it is not
    * empty. The solution will be written to the BT output port.
    */
   fp::Result<bool> doWork() override;
